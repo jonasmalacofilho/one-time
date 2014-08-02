b7ca530b8d720041892b00735726e0245e1cedbd
Jonas Malaco Filho <jonas@jonasmalaco.com>
2014-08-02 02:27:19 -0300

    Useless BFS

--- kml_area/KmlArea.hx
+++ kml_area/KmlArea.hx
@@ -1,22 +1,36 @@
-import Xml;
+import Lambda.iter;
 
 class KmlArea {
     
     static function main() {
         var kml = Xml.parse(Sys.stdin().readAll().toString());
-        process(kml);
+        bfs(kml, list);
+    }
+    
+    static function bfs(begin : Xml, f : Xml -> Void) {
+        var fifo = new List();
+        fifo.add(begin);
+        while (fifo.length > 0) {
+            var node = fifo.pop();
+            for (child in node)
+                switch (child.nodeType) {
+                case Xml.Element:
+                    fifo.add(child);
+                default:
+                    // noop
+                }
+            f(node);
+        } 
     }
 
-    static function process(kml : Xml) {
-        for (child in kml) {
-            //trace(child.nodeType);
-            switch (child.nodeType) {
-            case Xml.Element, Xml.Document:
-                trace('${child.nodeType}: ${child.nodeName}');
-                process(child);
-            default:
-                trace('${child.nodeType}: $child$$');
-            }
+    static function list(node : Xml) {
+        switch (node.nodeType) {
+        case Xml.Element:
+            trace('${node.nodeType}: ${node.nodeName}');
+            if (node.nodeName == "name")
+                trace('name = ${node.firstChild().nodeValue}');
+        default:
+            // noop
         }
     }
 
